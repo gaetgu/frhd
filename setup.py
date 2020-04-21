8cb0b61f5a272d0d8eff276b01142947f923d15c
gaetgu <47652746+gaetgu@users.noreply.github.com>
2020-04-21 17:44:39 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+versionFile = "VERSION"
+setup(name="frhdtools",
+    version=open(versionFile).read(),
+    description="Library to work with Free Rider HD Tracks",
+    long_description=open("README.rst").read(),
+    url="https://github.com/maxmillion18/frhdtools",
+    author="maxmillion18",
+    author_email="icantpostmyemailhere@gmail.com",
+    license="MIT License",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Code Generators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only"
+    ],
+    keywords="development freeriderhd freerider code track tracks",
+    packages=find_packages(exclude=["images"]),
+)
