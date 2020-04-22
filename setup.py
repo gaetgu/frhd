8ae68fd1a5fab0b42827d6d46c4ee7e53c333a02
gaetgu <47652746+gaetgu@users.noreply.github.com>
2020-04-21 20:50:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 versionFile = "VERSION"
 setup(name="frhd-python",
-    version=0.2,
+    version=0.4,
     description="Library to edit FRHD tracks",
     long_description=open("README.rst").read(),
     url="https://github.com/gaetgu/frhd",
