baab76926159ba5bd7abb2bf8cb3a412aede1b25
gaetgu <47652746+gaetgu@users.noreply.github.com>
2020-04-21 18:48:12 -0600

    Update Loader.py

--- frhd/Loader.py
+++ frhd/Loader.py
@@ -1,7 +1,3 @@
-#Loader.py - Freerider HD Track Loader
-#by maxmillion18
-#http://www.github.com/maxmillion18
-#http://www.freeriderhd.com/u/MaxwellNurzia
 
 import Decode as De #Decode.py - if you haven't already
                                    #go take a look ;)
