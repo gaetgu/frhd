38a300255dbd3e13e4c3ca672012a01da6aaa0f4
gaetgu <47652746+gaetgu@users.noreply.github.com>
2020-04-29 10:55:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 versionFile = "VERSION"
 setup(name="frhd-python",
-    version=3.4,
+    version=3.5,
     description="Library to edit FRHD tracks",
     long_description=open("README.rst").read(),
     url="https://github.com/gaetgu/frhd",
-    download_url="https://github.com/gaetgu/frhd/archive/v_3.4.tar.gz",
+    download_url="https://github.com/gaetgu/frhd/archive/v_3.5.tar.gz",
     install_requires=[
           'decode',
       ],
